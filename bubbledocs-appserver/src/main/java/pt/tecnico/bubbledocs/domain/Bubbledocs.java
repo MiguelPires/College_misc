caabf9fca679843240abf18dbc9a6c6fca91ddff
DavidIST <david.p.silva93@gmail.com>
2015-03-10 00:03:22 +0000

    User.java + Bubbledocs.java update

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Bubbledocs.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/Bubbledocs.java
@@ -1,9 +1,15 @@
 package pt.tecnico.bubbledocs.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bubbledocs extends Bubbledocs_Base {
     
     public Bubbledocs() {
         super();
     }
     
+	public ArrayList<Spreadsheet> findCreatedDocsByUser(User user, String name){
+		return user.findCreatedDocs(name);
+	}
 }
--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/User.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/domain/User.java
@@ -1,9 +1,28 @@
 package pt.tecnico.bubbledocs.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import pt.tecnico.bubbledocs.domain.Spreadsheet;
+
 public class User extends User_Base {
     
-    public User() {
-        super();
+    public User(String username, String name, String password) {
+		super();
+        setUsername(username);
+        setName(name);
+        setPassword(password);
     }
+	
+	User(){
+	}
     
+	public ArrayList<Spreadsheet> findCreatedDocs(String name){
+		ArrayList<Spreadsheet> documents = new ArrayList<Spreadsheet>();
+		for (Spreadsheet doc : this.getDocsCreatedSet())
+			if(doc.getName().equals(name))
+				documents.add(doc);
+				
+		return documents;
+	}
 }
+

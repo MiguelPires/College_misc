66645fcb985f786222f4eacf65d3b7d1c43caa50
Jane--c <joanacondeco@gmail.com>
2015-03-12 11:27:01 +0000

    Added UserNotFoundException; Implemented findUser(username) in Bubbledocs

---
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package pt.tecnico.bubbledocs.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException (String message){
+        super(message);
+    }
+    
+}

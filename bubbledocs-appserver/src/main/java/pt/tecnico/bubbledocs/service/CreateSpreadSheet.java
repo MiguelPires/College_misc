fa25854faee7cffa25f469bde50781c536096cb9
Ana Salta <ogawa.sano@gmail.com>
2015-03-19 17:21:10 +0000

    closes #4

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateSpreadSheet.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/CreateSpreadSheet.java
@@ -1,11 +1,17 @@
 package pt.tecnico.bubbledocs.service;
 
+import pt.tecnico.bubbledocs.domain.Spreadsheet;
+import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 
 // add needed import declarations
 
 public class CreateSpreadSheet extends BubbleDocsService {
     private int sheetId;  // id of the new sheet
+    private int rows;
+    private int columns;
+    private String token;
+    private String name;
 
     public int getSheetId() {
         return sheetId;
@@ -13,12 +19,18 @@ public class CreateSpreadSheet extends BubbleDocsService {
 
     public CreateSpreadSheet(String userToken, String name, int rows,
             int columns) {
-	// add code here
+        this.token = userToken;
+        this.name = name;
+        this.rows = rows;
+        this.columns = columns;
     }
 
     @Override
     protected void dispatch() throws BubbleDocsException {
-	// add code here
+        User user = getUserByToken(token);
+
+        Spreadsheet ss = createSpreadSheet(user, name, rows, columns);
+        sheetId = ss.getID();
     }
 
 }

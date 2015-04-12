39620d1aacf8b9daaad4ce635b61fdfd82f9fab1
DavidIST <david.p.silva93@gmail.com>
2015-04-12 21:00:47 +0100

    Closes #15

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/DeleteUser.java
@@ -2,8 +2,11 @@ package pt.tecnico.bubbledocs.service;
 
 import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.bubbledocs.exception.RemoteInvocationException;
 import pt.tecnico.bubbledocs.exception.UnauthorizedOperationException;
+import pt.tecnico.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+import pt.tecnico.bubbledocs.service.remote.IDRemoteServices;
 
 public class DeleteUser extends BubbleDocsService {
 
@@ -17,11 +20,18 @@ public class DeleteUser extends BubbleDocsService {
 
     @Override
     protected void dispatch() throws BubbleDocsException {
+    	IDRemoteServices remote = new IDRemoteServices();
         User user = getUserByToken(userToken);
 
         if (!isLoggedIn(user))
             throw new UserNotInSessionException();
         else if (user.isRoot()) {
+        	try{
+        	remote.removeUser(deleteUsername);
+        	} catch (RemoteInvocationException e) {
+        		throw new UnavailableServiceException();
+        	}
+
             getBubbledocs().removeUser(deleteUsername);
         } else
             throw new UnauthorizedOperationException();

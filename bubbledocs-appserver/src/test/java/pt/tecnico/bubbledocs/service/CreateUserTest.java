ff4215bbffefff0f06c6198c94316d630161e2e0
Ana Salta <ogawa.sano@gmail.com>
2015-04-12 16:05:25 +0100

    Closes #11

--- bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/CreateUserTest.java
+++ bubbledocs-appserver/src/test/java/pt/tecnico/bubbledocs/service/CreateUserTest.java
@@ -1,6 +1,10 @@
 package pt.tecnico.bubbledocs.service;
 
 import static org.junit.Assert.assertEquals;
+import mockit.Expectations;
+import mockit.Mock;
+import mockit.MockUp;
+import mockit.Mocked;
 
 import org.junit.Test;
 
@@ -10,6 +14,7 @@ import pt.tecnico.bubbledocs.exception.DuplicateUsernameException;
 import pt.tecnico.bubbledocs.exception.EmptyUsernameException;
 import pt.tecnico.bubbledocs.exception.UnauthorizedOperationException;
 import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+import pt.tecnico.bubbledocs.service.remote.IDRemoteServices;
 
 public class CreateUserTest extends BubbleDocsServiceTest {
 
@@ -17,22 +22,32 @@ public class CreateUserTest extends BubbleDocsServiceTest {
     private String ars;
 
     private static final String USERNAME = "ars";
-    private static final String PASSWORD = "ars";
+    private static final String EMAIL = "ars@tecnico.pt";
     private static final String ROOT_USERNAME = "root";
     private static final String USERNAME_DOES_NOT_EXIST = "no-one";
+    
+    private IDRemoteServices serviceId;
 
     @Override
     public void populate4Test() throws BubbleDocsException {
-        createUser(USERNAME, PASSWORD, "António Rito Silva");
+        createUser(USERNAME, EMAIL, "António Rito Silva");
         root = addUserToSession(ROOT_USERNAME);
         ars = addUserToSession("ars");
     }
 
     @Test
     public void success() {
-        CreateUser service = new CreateUser(root, USERNAME_DOES_NOT_EXIST, "jose", "José Ferreira");
+    	new MockUp<IDRemoteServices>() {
+  		   @Mock
+  		   public void createUser(String username, String email) {
+  			   //
+  		   }
+        };
+    	
+    	CreateUser service = new CreateUser(root, USERNAME_DOES_NOT_EXIST, "jose@tecnico.pt", "José Ferreira");
+        
         service.execute();
-
+        
         User user = getUserFromUsername(USERNAME_DOES_NOT_EXIST);
 
         assertEquals(USERNAME_DOES_NOT_EXIST, user.getUsername());
@@ -42,27 +57,45 @@ public class CreateUserTest extends BubbleDocsServiceTest {
 
     @Test(expected = DuplicateUsernameException.class)
     public void usernameExists() {
-        CreateUser service = new CreateUser(root, USERNAME, "jose", "José Ferreira");
+    	new MockUp<IDRemoteServices>() {
+    		@Mock
+     		public void createUser(String username, String email) {
+     			   throw new DuplicateUsernameException();
+    		}
+        };
+    	
+    	CreateUser service = new CreateUser(root, USERNAME, "jose@tecnico.pt", "José Ferreira");
+    	       
         service.execute();
+        
     }
 
     @Test(expected = EmptyUsernameException.class)
     public void emptyUsername() {
-        CreateUser service = new CreateUser(root, "", "jose", "José Ferreira");
+    	new MockUp<IDRemoteServices>() {
+    		@Mock
+     		public void createUser(String username, String email) {
+     			   throw new EmptyUsernameException();
+    		}
+        };
+    	
+    	CreateUser service = new CreateUser(root, "", "jose@tecnico.pt", "José Ferreira");
+        
         service.execute();
 
     }
 
     @Test(expected = UnauthorizedOperationException.class)
     public void unauthorizedUserCreation() {
-        CreateUser service = new CreateUser(ars, USERNAME_DOES_NOT_EXIST, "jose", "José Ferreira");
+        CreateUser service = new CreateUser(ars, USERNAME_DOES_NOT_EXIST, "jose@tecnico.pt", "José Ferreira");
+        
         service.execute();
     }
 
     @Test(expected = UserNotInSessionException.class)
     public void accessUsernameNotExist() {
         removeUserFromSession(root);
-        CreateUser service = new CreateUser(root, USERNAME_DOES_NOT_EXIST, "jose", "José Ferreira");
+        CreateUser service = new CreateUser(root, USERNAME_DOES_NOT_EXIST, "jose@tecnico.pt", "José Ferreira");
 
         service.execute();
     }

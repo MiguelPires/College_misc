9ba7199867f9deb985ddf06cb081fa8ac58f3ad8
MiguelPires <miguelpires94@gmail.com>
2015-04-13 13:06:09 +0100

    Closes #18

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportDocument.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/ExportDocument.java
@@ -6,34 +6,33 @@ import org.jdom2.output.XMLOutputter;
 import pt.tecnico.bubbledocs.domain.Spreadsheet;
 import pt.tecnico.bubbledocs.domain.User;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
+import pt.tecnico.bubbledocs.exception.RemoteInvocationException;
 import pt.tecnico.bubbledocs.exception.UnauthorizedOperationException;
+import pt.tecnico.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.bubbledocs.exception.UserNotInSessionException;
+import pt.tecnico.bubbledocs.service.remote.StoreRemoteServices;
 
 public class ExportDocument extends BubbleDocsService {
-
-    private  org.jdom2.Document docXML;
+    private org.jdom2.Document docXML;
     private String token;
     private int docId;
-    private byte[] XMLByte;
+    private StoreRemoteServices storeService;
 
-    public byte[] getXMLByte() {
-    	return XMLByte;
-    }
-    
-    public void convertToBytes(){
-    	XMLOutputter xml = new XMLOutputter();
+    public byte[] convertToBytes(org.jdom2.Document doc) {
+        XMLOutputter xml = new XMLOutputter();
         xml.setFormat(Format.getPrettyFormat());
-        String str = xml.outputString(docXML);
-        XMLByte = str.getBytes();
+        String str = xml.outputString(doc);
+        return str.getBytes();
     }
 
-    public  org.jdom2.Document getDocXML() {
+    public org.jdom2.Document getDocXML() {
         return docXML;
     }
 
     public ExportDocument(String userToken, int docId) {
         this.token = userToken;
         this.docId = docId;
+        this.storeService = new StoreRemoteServices();
     }
 
     @Override
@@ -42,16 +41,20 @@ public class ExportDocument extends BubbleDocsService {
 
         if (!isLoggedIn(user))
             throw new UserNotInSessionException();
-
+        
         Spreadsheet ss = getSpreadsheet(docId);
 
         if (ss.getCreator().equals(user) || ss.isWriter(user.getUsername())
                 || ss.isReader(user.getUsername())) {
 
             docXML = exportToXML(docId);
-            convertToBytes();
-
+            try {
+                storeService.storeDocument(token, ss.getName(), convertToBytes(docXML));
+            } catch (RemoteInvocationException e) {
+                throw new UnavailableServiceException("The storage service is unavailable");
+            }
         } else
             throw new UnauthorizedOperationException();
     }
 }
+

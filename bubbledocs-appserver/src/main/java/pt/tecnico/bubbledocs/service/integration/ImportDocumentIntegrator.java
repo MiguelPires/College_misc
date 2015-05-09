b24738d02fdcf8575698cc4337f308490ad3df88
VanessaFG <vanessa.gaspar@ist.utl.pt>
2015-05-09 14:25:15 +0100

    Update ImportDocumentIntegrator

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/integration/ImportDocumentIntegrator.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/integration/ImportDocumentIntegrator.java
@@ -1,5 +1,8 @@
 package pt.tecnico.bubbledocs.service.integration;
 
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
 import pt.tecnico.bubbledocs.domain.Spreadsheet;
 import pt.tecnico.bubbledocs.exception.BubbleDocsException;
 import pt.tecnico.bubbledocs.exception.RemoteInvocationException;
@@ -7,15 +10,17 @@ import pt.tecnico.bubbledocs.exception.UnavailableServiceException;
 import pt.tecnico.bubbledocs.service.remote.StoreRemoteServices;
 import pt.tecnico.bubbledocs.service.GetUsername4Token;
 import pt.tecnico.bubbledocs.service.ImportDocument;
+import pt.tecnico.bubbledocs.service.integration.CreateSpreadSheetIntegrator;
 
 
 public class ImportDocumentIntegrator extends BubbleDocsIntegrator {
     
   private String userToken;
   private String username;
-  private int docId;
-	
   private Spreadsheet doc;
+  private int docId;
+  private Spreadsheet newDoc;
+  private int newDocId;
   
   StoreRemoteServices remote;
   ImportDocument importService;
@@ -25,6 +30,13 @@ public class ImportDocumentIntegrator extends BubbleDocsIntegrator {
       this.docId = docId;
       this.userToken = userToken;
   }
+
+  public byte[] convertToBytes(org.jdom2.Document doc) {
+        XMLOutputter xml = new XMLOutputter();
+        xml.setFormat(Format.getPrettyFormat());
+        String str = xml.outputString(doc);
+        return str.getBytes();
+    }
 
   protected void dispatch() throws BubbleDocsException {
     remote = new StoreRemoteServices();
@@ -41,6 +53,18 @@ public class ImportDocumentIntegrator extends BubbleDocsIntegrator {
    	} catch (RemoteInvocationException e) {
    		throw new UnavailableServiceException();
    	}
+    
+    CreateSpreadSheetIntegrator createSpreadSheet = new CreateSpreadSheetIntegrator(userToken, doc.getName(), doc.getRows(), doc.getColumns());
+  
+    newDocId = createSpreadSheet.getID();
+    newDoc = getSpreadsheet(newDocId);
+    
+    try {
+      remote.storeDocument(userToken, newDoc.getName(), convertToBytes(importService.getDocXML()));
+    } catch (RemoteInvocationException e) {
+      throw new UnavailableServiceException("The storage service is unavailable");
+    }
+
   }
   
   public org.jdom2.Document getDocXML() {

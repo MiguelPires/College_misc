c698f1f0dc5acbd3c6a0e6475f361d111cbecaa3
MiguelPires <miguelpires94@gmail.com>
2015-05-10 15:58:55 +0100

    Closes #26

--- bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/remote/IDRemoteServices.java
+++ bubbledocs-appserver/src/main/java/pt/tecnico/bubbledocs/service/remote/IDRemoteServices.java
@@ -1,34 +1,69 @@
 package pt.tecnico.bubbledocs.service.remote;
 
+import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
+
+import javax.xml.registry.JAXRException;
+
+import pt.tecnico.Client;
 import pt.tecnico.bubbledocs.exception.DuplicateEmailException;
 import pt.tecnico.bubbledocs.exception.DuplicateUsernameException;
 import pt.tecnico.bubbledocs.exception.InvalidEmailException;
 import pt.tecnico.bubbledocs.exception.InvalidUsernameException;
 import pt.tecnico.bubbledocs.exception.LoginBubbleDocsException;
 import pt.tecnico.bubbledocs.exception.RemoteInvocationException;
+import pt.tecnico.bubbledocs.exception.UnknownBubbleDocsUserException;
+import pt.ulisboa.tecnico.sdis.id.ws.AuthReqFailed_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.EmailAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidEmail_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.InvalidUser_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserAlreadyExists_Exception;
+import pt.ulisboa.tecnico.sdis.id.ws.UserDoesNotExist_Exception;
 
 public class IDRemoteServices {
 
-    public void createUser(String username, String email) throws InvalidUsernameException,
-                                                         DuplicateUsernameException,
-                                                         DuplicateEmailException,
-                                                         InvalidEmailException,
-                                                         RemoteInvocationException {
-        // TODO : the connection and invocation of the remote service
+    public void createUser(String username, String email) {
+        try {
+            Client.getInstanceID().createUser(username, email);
+        } catch (JAXRException e) {
+            throw new RemoteInvocationException(e.getMessage());
+        } catch (InvalidEmail_Exception e) {
+            throw new InvalidEmailException(e.getMessage());
+        } catch (InvalidUser_Exception e) {
+            throw new InvalidUsernameException(e.getMessage());
+        } catch (EmailAlreadyExists_Exception e) {
+            throw new DuplicateEmailException(e.getMessage());
+        } catch (UserAlreadyExists_Exception e) {
+            throw new DuplicateUsernameException(e.getMessage());
+        }
     }
 
-    public void loginUser(String username, String password) throws LoginBubbleDocsException,
-                                                           RemoteInvocationException {
-        // TODO : the connection and invocation of the remote service
+    public void loginUser(String username, String password) {
+        try {
+            Client.getInstanceID().requestAuthentication(username, parseBase64Binary(password));
+        } catch (JAXRException e) {
+            throw new RemoteInvocationException();
+        } catch (AuthReqFailed_Exception e) {
+            throw new LoginBubbleDocsException();
+        }
     }
 
-    public void removeUser(String username) throws LoginBubbleDocsException,
-                                           RemoteInvocationException {
-        // TODO : the connection and invocation of the remote service
+    public void removeUser(String username) {
+        try {
+            Client.getInstanceID().removeUser(username);
+        } catch (JAXRException e) {
+            throw new RemoteInvocationException();
+        } catch (UserDoesNotExist_Exception e) {
+            throw new UnknownBubbleDocsUserException(e.getMessage());
+        }
     }
 
-    public void renewPassword(String username) throws LoginBubbleDocsException,
-                                              RemoteInvocationException {
-        // TODO : the connection and invocation of the remote service
+    public void renewPassword(String username) {
+        try {
+            Client.getInstanceID().renewPassword(username);
+        } catch (JAXRException e) {
+            throw new RemoteInvocationException();
+        } catch (UserDoesNotExist_Exception e) {
+            throw new UnknownBubbleDocsUserException(e.getMessage());
+        }
     }
 }

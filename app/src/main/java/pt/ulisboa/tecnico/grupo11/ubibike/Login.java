111715c521fa4aca005615d5454f9d39575627f7
PAAMatias <matiasrf600@gmail.com>
2016-03-14 19:39:45 +0000

    Register Activity and Login Activity finished
    UI:
    -Register Activity (finished)
    -Login Activity (finished)
    -Home Activity (not finished)
    -Contacts Activity (finished?)
    
    Missing:
    - Swipe from the Home activity to the contacts activity
    - Test communication between client and server (users, password, etc)
    - Popup options when we select a user from the ListView in Contacts
    Activity
    - ...

--- app/src/main/java/pt/ulisboa/tecnico/grupo11/ubibike/Login.java
+++ app/src/main/java/pt/ulisboa/tecnico/grupo11/ubibike/Login.java
@@ -3,13 +3,28 @@ package pt.ulisboa.tecnico.grupo11.ubibike;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 public class Login extends AppCompatActivity {
+
+    static final String serverUrl = "http://localhost:8000";
 
     Button loginBtn;
 
@@ -19,8 +34,8 @@ public class Login extends AppCompatActivity {
     TextView registerTxt;
 
     /*
-        Fazer onclicklistener para butao login
-        Fazer toast a dizer o que estava errado no login
+        Testarmos
+        Vermos se os hashs sao iguais e tirar um deles
      */
 
     @Override
@@ -35,13 +50,107 @@ public class Login extends AppCompatActivity {
         registerTxt = (TextView)findViewById(R.id.registerTxt);
     }
 
-    public void login (View view)
-    {
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
+    public void login (View view) throws IOException, NoSuchAlgorithmException {
+        final Thread connectionThread  = new Thread(new Runnable() {
+            public void run() {
+                String username = usernameLogin.getText().toString();
+                String password = passwordLogin.getText().toString();
+                String url = serverUrl+"/users/"+username;
+                int responseCode=0;
+                HttpURLConnection readUsersConn;
+                URL usersUrl = null;
+                try {
+                    usersUrl = new URL(url);
+                    readUsersConn = (HttpURLConnection) usersUrl.openConnection();
+                    readUsersConn.setRequestMethod("GET");
+                    responseCode = readUsersConn.getResponseCode();
+                    Log.d("TAG", "Server response code: " + responseCode);
+                } catch (IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(Login.this, "Network Error! Check your connection.", Toast.LENGTH_SHORT);
+                        }
+                    });
+                    return;
+                }
+                if(responseCode==200)
+                {
+                    MessageDigest md = null;
+                    try {
+                        md = MessageDigest.getInstance("SHA-256");
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
+                    md.update(password.getBytes());
+                    byte byteData[] = md.digest();
+                    Arrays.toString(byteData);
+                    String hashBase64 = Base64.encodeToString(byteData, Base64.NO_WRAP | Base64.URL_SAFE);
+                    StringBuffer sb = new StringBuffer();
+                    for (int i = 0; i < byteData.length; i++)
+                    {
+                        sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+                    }
+                    Log.d("TAG", "hashBase64 text: " + hashBase64 + " | stringbuffer text: " + sb.toString());
+                    InputStream is = null;
+                    byte[] buffer = null;
+                    try {
+                        is = readUsersConn.getInputStream();
+                        //BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                        String line;
+                        url+="/hash";
+                        usersUrl = new URL(url);
+                        readUsersConn = (HttpURLConnection) usersUrl.openConnection();
+                        readUsersConn.setRequestMethod("GET");
+                        responseCode = readUsersConn.getResponseCode();
+                        buffer  = new byte[readUsersConn.getContentLength()];
+                        is.read(buffer);
+                        //line = rd.readLine();
+                        //rd.close();
+                    } catch (IOException e) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(Login.this, "Network Error! Check your connection.", Toast.LENGTH_SHORT);
+                            }
+                        });
+                        return;
+                    }
+                    if ( responseCode==200 && Arrays.equals(buffer, byteData))
+                    {
+                        Intent intent = new Intent(Login.this, Home.class);
+                        startActivity(intent);
+                    }
+                    else
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                usernameLogin.setText("");
+                                passwordLogin.setText("");
+                                Toast.makeText(Login.this, "Internal Error!", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                }
+                else
+                {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            usernameLogin.setText("");
+                            passwordLogin.setText("");
+                            Toast.makeText(Login.this, "Wrong user data! Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
+        connectionThread.start();
+
     }
 
-    public void register (View view)
+    public void loadRegister (View view)
     {
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
--- app/src/main/java/pt/ulisboa/tecnico/grupo11/ubibike/Register.java
+++ app/src/main/java/pt/ulisboa/tecnico/grupo11/ubibike/Register.java
@@ -1,11 +1,22 @@
 package pt.ulisboa.tecnico.grupo11.ubibike;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class Register extends AppCompatActivity {
 
@@ -30,17 +41,76 @@ public class Register extends AppCompatActivity {
         usernameTb = (EditText)findViewById(R.id.regUsernameTb);
         passwordTb = (EditText)findViewById(R.id.regPasswordTb);
         confirmPasswordTb = (EditText)findViewById(R.id.regConfirmPassTb);
+    }
 
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!(confirmPasswordTb.getText().equals(passwordTb.getText())))
+    public void register (View view)
+    {
+        final Thread connectionThread  = new Thread(new Runnable() {
+            public void run() {
+                if (!(confirmPasswordTb.getText().toString().equals(passwordTb.getText().toString())))
                 {
-                    Toast.makeText(Register.this, "Password does not match!", Toast.LENGTH_SHORT).show();
-                    passwordTb.setText("");
-                    confirmPasswordTb.setText("");
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(Register.this, "Password does not match!", Toast.LENGTH_SHORT).show();
+                            passwordTb.setText("");
+                            confirmPasswordTb.setText("");
+                        }
+                    });
+                    return;
+                }
+                URL newUserUrl = null;
+                int responseCode = 0;
+                try {
+                    newUserUrl = new URL(Login.serverUrl+"/users/"+usernameTb.getText().toString());
+                    MessageDigest md = null;
+                    try {
+                        md = MessageDigest.getInstance("SHA-256");
+                    } catch (NoSuchAlgorithmException e) {
+                        e.printStackTrace();
+                    }
+                    md.update(passwordTb.getText().toString().getBytes());
+                    byte byteData[] = md.digest();
+                    Arrays.toString(byteData);
+                    // String hashBase64 = Base64.encodeToString(byteData, Base64.NO_WRAP | Base64.URL_SAFE);
+                    // byte[] data = hashBase64.getBytes();
+
+                    HttpURLConnection createUserConn = (HttpURLConnection) newUserUrl.openConnection();
+                    createUserConn.setDoOutput(true);
+                    createUserConn.setRequestMethod("PUT");
+
+                    DataOutputStream wr = new DataOutputStream(createUserConn.getOutputStream());
+                    wr.write(byteData);
+                    wr.close();
+
+                    responseCode = createUserConn.getResponseCode();
+                    if ( responseCode == 400)
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(Register.this, "Username already in use!", Toast.LENGTH_SHORT);
+                                usernameTb.setText("");
+                                passwordTb.setText("");
+                            }
+                        });
+                    }
+                    else
+                    {
+                        Intent intent = new Intent(Register.this, Home.class);
+                        startActivity(intent);
+                    }
+
+                } catch (java.io.IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(Register.this, "Network Error! Check your connection.", Toast.LENGTH_SHORT);
+                        }
+                    });
                 }
             }
         });
+        connectionThread.start();
     }
 }

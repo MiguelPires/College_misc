ff080ec0b75e3bee1bb94a4f008b9eda46fe2617
Miguel Pires <miguelpires94@gmail.com>
2016-10-25 17:57:20 +0100

    Improved script and added unigrams

--- run.sh
+++ run.sh
@@ -1,18 +1,36 @@
 CORP_DIR=Corpora/treino
+PROFILE_SIZE=100
 
-if [ $# != 0 ] && [ $1 = "-c" ]; then
-	rm -f *-bigrams.txt temp-*.txt
+if [[ $# == 0 ]] || [[ $1 == help ]]; then
+	echo "run.sh <command>"
+	echo "clean: deletes the author n-grams and profiles" 
+	echo "generate: generates the authors' n-grams and profiles"
+	echo "help: displays this list"
 	exit
 fi
 
-for AUTHOR in $( ls $CORP_DIR ); do
-count=0
-	for i in $CORP_DIR/$AUTHOR/*.txt; do
-	echo "$i"
-	ngram-count -sort -write-order 2 -text "$i" > temp-$count.txt
-	let count=count+1
+# Deletes the n-grams and profiles
+if [[ $1 == "clean" ]]; then
+	echo "Cleaning directory"
+	rm -f *-unigrams.txt *-bigrams.txt temp-*.txt
+	exit
+fi
+
+# Generates the author n-grams and profiles
+if [[ $1 == "generate" ]]; then
+
+	for AUTHOR in $( ls $CORP_DIR ); do
+		count=0
+		echo "Generating n-grams for author '$AUTHOR'"
+		for i in $CORP_DIR/$AUTHOR/*.txt; do
+			ngram-count -sort -write1 temp-uni-$count.txt -write2 temp-bi-$count.txt -text "$i"
+			let count=count+1
+		done
+		
+		ngram-merge -write $AUTHOR-unigrams.txt temp-uni-*.txt	
+		ngram-merge -write $AUTHOR-bigrams.txt temp-bi-*.txt
+		rm temp-*.txt
 	done
-	
-	ngram-merge -write $AUTHOR-bigrams.txt temp-*.txt
-	rm temp-*.txt
-done
+fi
+
+

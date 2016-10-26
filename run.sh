d3cab95691d527d032e86f068f3f94e1f89bc06b
Miguel Pires <miguelpires94@gmail.com>
2016-10-26 11:05:02 +0100

    Script applies model to text and picks best author

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
-CORP_DIR=Corpora/treino
+TRAIN_DIR=Corpora/treino
+#TEST_DIR=Corpora/teste/500Palavras
+TEST_DIR=Corpora/teste/1000Palavras
 PROFILE_SIZE=100
 
-if [[ $# == 0 ]] || [[ $1 == help ]]; then
+if [[ $1 == help ]]; then
 	echo "run.sh <command>"
 	echo "clean: deletes the author n-grams and profiles" 
 	echo "generate: generates the authors' n-grams and profiles"
@@ -10,27 +12,60 @@ if [[ $# == 0 ]] || [[ $1 == help ]]; then
 fi
 
 # Deletes the n-grams and profiles
-if [[ $1 == "clean" ]]; then
+
+if [[ $# == 0 ]] || [[ $1 == "clean" ]]; then
 	echo "Cleaning directory"
-	rm -f *-unigrams.txt *-bigrams.txt temp-*.txt
-	exit
+	rm -f *arpa.txt temp-*.txt
+	
+	if [[ $1 == "clean" ]]; then
+		exit
+	fi
 fi
 
 # Generates the author n-grams and profiles
-if [[ $1 == "generate" ]]; then
-
-	for AUTHOR in $( ls $CORP_DIR ); do
+if [[ $# == 0 ]] || [[ $1 == "generate" ]]; then
+	for AUTHOR in $( ls $TRAIN_DIR ); do
 		count=0
 		echo "Generating n-grams for author '$AUTHOR'"
-		for i in $CORP_DIR/$AUTHOR/*.txt; do
-			ngram-count -sort -write1 temp-uni-$count.txt -write2 temp-bi-$count.txt -text "$i"
+	
+		for i in $TRAIN_DIR/$AUTHOR/*.txt; do
+			ngram-count -sort -order 2 -text "$i" -addsmooth 0 -write temp-$AUTHOR-$count.txt
 			let count=count+1
 		done
 		
-		ngram-merge -write $AUTHOR-unigrams.txt temp-uni-*.txt	
-		ngram-merge -write $AUTHOR-bigrams.txt temp-bi-*.txt
-		rm temp-*.txt
+		# create author profile
+		ngram-merge -write $AUTHOR-count.txt temp-$AUTHOR-*.txt
+		ngram-count -sort -read $AUTHOR-count.txt -addsmooth 0 -lm $AUTHOR-arpa.txt 
+		rm temp-*.txt $AUTHOR-count.txt
 	done
+
+	if [[ $1 == "generate" ]]; then
+		exit
+	fi
 fi
 
+if [[ $# == 0 ]] || [[ $1 == "run" ]]; then
+	for TEST_FILE in $( ls $TEST_DIR ); do
+		BEST_PPL=-1
+		BEST_AUTHOR=""
 
+		for AUTHOR in $( ls $TRAIN_DIR ); do	
+			# if we want to use only the N most frequent ngrams
+			#head -n $PROFILE_SIZE $AUTHOR-bigrams.txt $AUTHOR-profile
+
+			# apply the model to the text and extract the perplexity
+			PPL="$(ngram -lm $AUTHOR-arpa.txt -ppl $TEST_DIR/$TEST_FILE  | grep -o "ppl= [0-9]*" | grep -o "[0-9]*")"
+
+			if [[ $BEST_PPL == -1 ]] || [[ $PPL -lt $BEST_PPL ]]; then
+				BEST_AUTHOR=$AUTHOR
+				BEST_PPL=$PPL
+			fi				
+		done
+		
+		echo "The author of the text '$TEST_FILE' is $BEST_AUTHOR (ppl = $BEST_PPL)"
+	done
+	
+	if [[ $1 == "run" ]]; then
+		exit
+	fi
+fi
